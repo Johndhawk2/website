62abecb33e7bab22dd859d1774ef9961225b3f0e
Johndhawk2 <Johndhawk2@users.noreply.github.com>
2019-06-25 22:29:12 +0100

    Update

--- js/Weather1.js
+++ js/Weather1.js
@@ -2,9 +2,15 @@ const weatherKey = '3b74fe5b0c7443428c1125904192406';
 const locCheck  = 'Bromley';
 const DoWList = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 const MonList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const locList = ["Amsterdam", "Barcelona", "Belfast", "Bordeaux", 
+				 "Brighton", "Bristol", "Bromley", "California",
+				 "Cannes", "Córdoba", "Durham", "Dublin",
+				 "Edinburgh", "Glasgow", "Leeds", "Lille",
+				 "Liverpool", "Marseilles", "Manchester", "Newcastle",
+				 "New york", "Paris", "Seville", "Southampton"];
 
 var wForecast = [];
-var locForecast = ["Bromley", "Durham", "Paris", "New York", "Sidney", "Berlin", "Moscow", "Hong Kong", "Deli", "Huston"];
+var locForecast = [];
 
 function getForecast(i, jsonResponse){
 	this.date = jsonResponse.forecast.forecastday[i].date;
@@ -24,28 +30,44 @@ function getLocForecast(desiredLocation){
 }
 
 function load(){
-	for(var j=0; j<7; j++)cardBuild(locForecast[j], j);
+	for(var i=0;i<locList.length;i++){
+		document.getElementById("location").innerHTML += `<option value="${locList[i]}">${locList[i]}</option>`
+	}
 }
 
 function refresh(){
-	for(var j=0; j<7; j++)getWeatherForecast(locForecast[j], j);
+//	for(var j=0; j<locForecast.length; j++)getWeatherForecast(locForecast[j]);
+	addCard();
 }
 
-async function getWeatherForecast(desiredLocation, cardNo){
+function addCard(){
+	var location = document.getElementById("location").value;
+	if(locForecast.indexOf(location) == -1){
+		locForecast.push(location);
+		cardBuild(location);
+	}
+}
+
+function deleteCard(location){
+	locForecast.splice(locForecast.indexOf(location),1);
+	document.getElementById("weatherCard" + location).remove();
+}
+
+async function getWeatherForecast(desiredLocation){
 	const response = await fetch(`https://api.apixu.com/v1/forecast.json?key=${weatherKey}&q=${desiredLocation}&days=7`);
 	const jsonResponse = await response.json();
 	for(i=0; i<7; i++){
 		var item = new getForecast(i, jsonResponse);
 		wForecast[i] = item;
-		try{document.getElementById("weatherCard" + cardNo).getElementsByClassName("Day" + i)[0].getElementsByClassName("DoW")[0].innerHTML = wForecast[i].day}catch{};
-		try{document.getElementById("weatherCard" + cardNo).getElementsByClassName("Day" + i)[0].getElementsByClassName("Icon")[0].src = wForecast[i].icon}catch{};
-		try{document.getElementById("weatherCard" + cardNo).getElementsByClassName("Day" + i)[0].getElementsByClassName("Temp")[0].innerHTML = "Avg:" + wForecast[i].avgtemp_c + "°C"}catch{};
-		try{document.getElementById("weatherCard" + cardNo).getElementsByClassName("Day" + i)[0].getElementsByClassName("MaxTemp")[0].innerHTML = "Max:" + wForecast[i].maxtemp_c + "°C"}catch{};
-		try{document.getElementById("weatherCard" + cardNo).getElementsByClassName("Day" + i)[0].getElementsByClassName("MinTemp")[0].innerHTML = "Min:" + wForecast[i].mintemp_c + "°C"}catch{};
-		try{document.getElementById("weatherCard" + cardNo).getElementsByClassName("Day" + i)[0].getElementsByClassName("Text")[0].innerHTML = wForecast[i].text}catch{};
+		try{document.getElementById("weatherCard" + desiredLocation).getElementsByClassName("Day" + i)[0].getElementsByClassName("DoW")[0].innerHTML = wForecast[i].day}catch{};
+		try{document.getElementById("weatherCard" + desiredLocation).getElementsByClassName("Day" + i)[0].getElementsByClassName("Icon")[0].src = wForecast[i].icon}catch{};
+		try{document.getElementById("weatherCard" + desiredLocation).getElementsByClassName("Day" + i)[0].getElementsByClassName("Temp")[0].innerHTML = "Avg:" + wForecast[i].avgtemp_c + "°C"}catch{};
+		try{document.getElementById("weatherCard" + desiredLocation).getElementsByClassName("Day" + i)[0].getElementsByClassName("MaxTemp")[0].innerHTML = "Max:" + wForecast[i].maxtemp_c + "°C"}catch{};
+		try{document.getElementById("weatherCard" + desiredLocation).getElementsByClassName("Day" + i)[0].getElementsByClassName("MinTemp")[0].innerHTML = "Min:" + wForecast[i].mintemp_c + "°C"}catch{};
+		try{document.getElementById("weatherCard" + desiredLocation).getElementsByClassName("Day" + i)[0].getElementsByClassName("Text")[0].innerHTML = wForecast[i].text}catch{};
 	}
-	document.getElementById("weatherCard" + cardNo).getElementsByClassName("Date")[0].innerHTML = "Weekly forecast: " + dateInvert(wForecast[0].date);
-	document.getElementById("weatherCard" + cardNo).getElementsByClassName("Place")[0].innerHTML = wForecast[0].place;
+	document.getElementById("weatherCard" + desiredLocation).getElementsByClassName("Date")[0].innerHTML = "Weekly forecast: " + dateInvert(wForecast[0].date);
+	document.getElementById("weatherCard" + desiredLocation).getElementsByClassName("Place")[0].innerHTML = wForecast[0].place;
 	console.log(wForecast);
 	console.log(jsonResponse);
 	return jsonResponse;
@@ -55,17 +77,17 @@ function dateInvert(date){
 	return date.split('-').reverse().join('-');
 }
 
-function cardBuild(desiredLocation, idNum){
+function cardBuild(desiredLocation){
 	document.getElementsByClassName("weatherCards")[0].innerHTML +=`
-	<div class="weatherCard" id="weatherCard${idNum}">
+	<div class="weatherCard" id="weatherCard${desiredLocation}">
 	</div>`;
-	document.getElementById("weatherCard" + idNum).innerHTML=`
+	document.getElementById("weatherCard" + desiredLocation).innerHTML=`
 	<div id="Header">
 		<h1 class="Date"></h1>
 		<p class="Place"></p>
 	</div>`
 	for(i=0;i<7;i++){
-		document.getElementById("weatherCard" + idNum).innerHTML+=`
+		document.getElementById("weatherCard" + desiredLocation).innerHTML+=`
 		<div class="Day Day${i}">
 			<p class="DoW"></p>
 			<img src="" class="Icon temp">
@@ -76,5 +98,5 @@ function cardBuild(desiredLocation, idNum){
 			<p class="Text temp"></p>
 		</div>`
 	}
-	getWeatherForecast(desiredLocation, idNum);
+	getWeatherForecast(desiredLocation);
 }
