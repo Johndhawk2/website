70d1bd4860a3936e173a18c1847b93b798519b0a
Johndhawk2 <Johndhawk2@users.noreply.github.com>
2020-06-01 14:39:34 +0100

    Improved Phone Navigation
    Added side bar navigation for smaller screens

---
+++ johnhawk.js
@@ -0,0 +1,11 @@
+function dropdownCollapse(){
+	$('.collapse').collapse('hide');
+}
+
+function openNav() {
+	document.getElementById("navbarSide").style.width = "100%";
+}
+
+function closeNav() {
+	document.getElementById("navbarSide").style.width = "0";
+}
