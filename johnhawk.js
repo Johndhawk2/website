a805c46c64e3400b193b85cb3228c3238e9b8a64
John Hawk <john@johnhawk.tech>
2020-07-06 23:59:48 +0100

    Mobile scrolling fix (hopefully)
    Replaced all references to viewport height with custom variable --hv which is 1% of the inner viewport height. This should take into account the url bar at the top of mobile browsers, and display the page correctly.

--- johnhawk.js
+++ johnhawk.js
@@ -14,6 +14,12 @@ $(document).ready(function(){
 	$('.nav-tabs a:first').tab('show')
 	imgFade();
 	addLoadEvent(preloader);
+	
+	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+	let vh = window.innerHeight * 0.01;
+	// Then we set the value in the --vh custom property to the root of the document
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 	/*$("html, body, .scroll").css({
 		height: $(window).height()
 	});*/
