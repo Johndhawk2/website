718b5f4e73e0f7e33cd4dee5a7146fe7c5b05ce4
John Hawk <admin@johnhawk.tech>
2021-08-27 15:14:29 +0100

    Update main.js

--- Biomex Testing Utility/main.js
+++ Biomex Testing Utility/main.js
@@ -1,5 +1,5 @@
 var ble = {
-	//devName: "HMSoft",
+	devName: "HMSoft",
 	sendRecService: "0000ffe0-0000-1000-8000-00805f9b34fb",
 	sendRecCharacteristic: "0000ffe1-0000-1000-8000-00805f9b34fb",
 	msgRec: ""
